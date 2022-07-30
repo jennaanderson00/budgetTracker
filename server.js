abe877cbf194904d3c63a4967ab3175ebc23a230
0ffpudding <jennaanderson00@gmail.com>
2022-07-29 20:24:06 -0500

    installed dependencies, created gitignore file, added service worker

--- server.js
+++ server.js
@@ -1,3 +1,19 @@
+// when users input an expense or deposit without internet connection, they get a notification that they have added an expense or depost
+// when internet connection is reestablished, the deposits or expenses added when offline are added to their transaction history and their totals are updated
+// idb.js file added to public/js
+// service worker added to root of the public directory
+// manifest.json contains the following properties:
+  // name
+  // short_name
+  // icons
+  // theme_color
+  // background_color
+  // start_url
+  // display
+// need to manually create the manifest.json file
+// uses mongodb
+// deploy with heroku and mongodb atlas
+
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
